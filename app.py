4704f8f2f4e564a2b6e5e637e94ab2e3afb9f34e
Wallysson de Lima Silva <wallysson.silva@academico.ifrn.edu.br>
2017-02-15 17:58:26 -0300

    update base. fix urls

--- app.py
+++ app.py
@@ -1,14 +1,23 @@
 from flask.ext.api import FlaskAPI
 from src import acompound_interest
+
 app = FlaskAPI(__name__)
 
-@app.route('/test')
-def test():
-	return {'teste': 'teste '}
+@app.route('/amount/<string:type>/<float:capital>/<float:interest>/<float:time_interest>')
+def amount(capital, interest, time_interest):
+	pass
 
-@app.errorhandler(404)
-def teste():
-	return 'ola'
+@app.route('/interest/<string:type>/<float:capital>/<float:interest>/<float:time_interest>')
+def capital(amount, interest, time_interest):
+	pass
+
+@app.route('/interest/<string:type>/<float:amount>/<float:capital>/<float:time_interest>')
+def interest(amount, capital, time_interest):
+	pass
+
+@app.route('/timeinterest/<string:type>/<float:amount>/<float:capital>/<float:interest>')
+def time_interest(amount, capital,interest):
+	pass
 
 if __name__ == '__main__':
 	app.run()
