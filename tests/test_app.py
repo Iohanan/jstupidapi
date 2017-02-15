d86ca718f401e60f70ab6bee4cab924ab3c53beb
wallyssonlima <wallyssonlds@gmail.com>
2017-02-15 15:33:31 -0300

    add acompound interest

--- tests/test_app.py
+++ tests/test_app.py
@@ -14,13 +14,16 @@ class TestAcompound(unittest.TestCase):
         self.assertEqual(206.06, Acompound.calculate_amount(200, 0.01, 3))
                                                                                                                 
     def test_calculate_capital(self):
-        pass
-    
+        self.assertEqual(505.00, Acompound.calculate_capital(500, 0.01, 1))
+        self.assertEqual(510.05, Acompound.calculate_capital(500, 0.01, 2))
+        self.assertEqual(12500.00, Acompound.calculate_capital(15237.43, 0.01, 10))
+        self.assertEqual(536.07, Acompound.calculate_capital(500, 0.01, 8))
+ 
     def test_calculate_interest(self):
-        pass
+        self.assertEqual(0.0230, Acompound.calculate_interest(5862.72,5000, 7))
     
     def test_calculate_time_interest(self):
-        pass
+        self.assertEqual(8.00, Acompound.calculate_time_interest(84486.94, 75000, 0.015))
 
 if __name__ == '__main__':
     unittest.main()
