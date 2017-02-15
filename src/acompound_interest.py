f00c66fd2ca06e7e37000d63d552e2271154f026
wallyssonlima <wallyssonlds@gmail.com>
2017-02-15 10:29:27 -0300

    fix tests :)

--- src/acompound_interest.py
+++ src/acompound_interest.py
@@ -1,11 +1,23 @@
-from math import pow
 
 class Acompound(object):
 	''' class for calculate parts about acompound interest '''
 	@staticmethod
-	def calculate_amount(capital_rate, interest_rate, time_interest):
-		_amount = capital_rate * pow(1+interest_rate, 2)
-		return _amount
+	def calculate_amount(capital, interest, time_interest):
+		_amount = capital * pow(1+interest, time_interest)
+		return float('%.2f' % _amount)
+	
+	@staticmethod
+	def calculate_capital(amount, interest, time_interest):
+		_capital = amount / pow(1+interest, time_interest)
+		return float('%.2f' % _capital)
+	
+	@staticmethod
+	def calculate_interest():
+		pass                                                                                                         
+	
+	@staticmethod
+	def calculate_time_interest():
+		pass
 
 
 if __name__ == '__main__':
--- tests/test_app.py
+++ tests/test_app.py
@@ -1,2 +1,26 @@
-from src import acompound_interest, interest_exceptions
-import pytest
+import sys
+from os import path
+sys.path.append(path.join('..'))
+
+from src.acompound_interest import Acompound
+
+import unittest
+
+class TestAcompound(unittest.TestCase):
+    '''Tests for Acompound class'''
+    def test_calculate_amount(self):
+        self.assertEqual(12363.61, Acompound.calculate_amount(12000, 0.01, 3))
+        self.assertEqual(5306.04, Acompound.calculate_amount(5000, 0.02, 3))
+        self.assertEqual(206.06, Acompound.calculate_amount(200, 0.01, 3))
+                                                                                                                
+    def test_calculate_capital(self):
+        pass
+    
+    def test_calculate_interest(self):
+        pass
+    
+    def test_calculate_time_interest(self):
+        pass
+
+if __name__ == '__main__':
+    unittest.main()
